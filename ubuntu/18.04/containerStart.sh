899026130e93f9d065abd60883c89b9bca764f0a
Mark Weaver <mark@blushingpenguin.com>
2019-02-05 08:33:00 +0000

    remove old ::1 attempt

--- ubuntu/18.04/containerStart.sh
+++ ubuntu/18.04/containerStart.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
 service mongodb start
-# stuff still tries to connect to ::1 even
-# with "::1 localhost" removed from hosts
-# cat /etc/hosts |grep -v :: > hosts
-# cat hosts > /etc/hosts
-# rm hosts
 ./start.sh
