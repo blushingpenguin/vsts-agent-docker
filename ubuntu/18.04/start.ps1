a558632482a902560a559baf1ec5544fbaf6b391
Mark Weaver <mark@blushingpenguin.com>
2019-07-25 13:33:27 +0000

    more tools

--- ubuntu/18.04/start.ps1
+++ ubuntu/18.04/start.ps1
@@ -14,4 +14,4 @@ $VSTS_TOKEN = Get-Secret -Name "VSTS-TOKEN"
 $VSTS_ACCOUNT = Get-Secret -Name "VSTS-ACCOUNT"
 
 # docker run --env VSTS_TOKEN=$VSTS_TOKEN --env VSTS_ACCOUNT=$VSTS_ACCOUNT --env VSTS_POOL="Azure Container Instance" --env VSTS_AGENT="ltest" -m 3.5G -it vsts-agent:latest bash
-docker run --env VSTS_TOKEN=$VSTS_TOKEN --env VSTS_ACCOUNT=$VSTS_ACCOUNT --env VSTS_POOL="Azure Container Instance" --env VSTS_AGENT="ltest" -m 3.5G -d --rm vsts-agent
+docker run -v /var/run/docker.sock:/var/run/docker.sock --env VSTS_TOKEN=$VSTS_TOKEN --env VSTS_ACCOUNT=$VSTS_ACCOUNT --env VSTS_POOL="Azure Container Instance" --env VSTS_AGENT="ltest" -m 3.5G -d --rm vsts-agent
